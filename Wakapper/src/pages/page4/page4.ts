4a135360fafccc611c79a4bbd815fb25f2f9aad5
寝落ち <ueken0307@hotmail.co.jp>
2018-01-23 19:02:06 +0900

    Revert " [update]update Page4 & add TimeTablePage "

--- Wakapper/src/pages/page4/page4.ts
+++ Wakapper/src/pages/page4/page4.ts
@@ -1,42 +1,97 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-import {TimeTablePage} from '../time-table/time-table'
-
 @Component({
   selector: 'page-page4',
   templateUrl: 'page4.html'
 })
 
 export class Page4 {
-  traffic: string = "boat";
+  selectedItem: any;
+  icons: string[];
 
-  boat: string[];
+  items: Array<{ title: string, note: string }>;
+
+  //表示する文字列
+  title: string[];
+  note: string[];
+
+  //交通手段
+  traffic: string[];
+  //駅
   train: string[];
+  //バス停
   bus: string[];
 
-
-  selectedItem: any;
-  data: Array<{ traffic: Number, place: Number }>;
-
+  //Flag : 時刻表を表示しているかのflag
+  flag: number = 1;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.selectedItem = navParams.get('item');
 
-    //渡船場データ取得
-    this.boat = ['若松渡場', '戸畑渡場'];
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('title');
+    if (this.selectedItem == undefined) {
+      this.selectedItem = '運行表'
+    }
 
-    //駅データ取得
+    this.traffic = ['渡船', '電車', 'バス'];
     this.train = ['若松駅', '奥洞海駅', '藤ノ木駅', '二島駅'];
-
-    //バス停データ取得
     this.bus = ['バス停1', 'バス停2', 'バス停3', 'バス停4'];
 
 
+    /*
+    // Let's populate this page with some filler content for funzies
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+    */
+
+
+    this.title = this.traffic;
+    if (this.selectedItem == this.traffic[0]) {
+      //時刻表ファイルを読み込む
+      //テスト用のやつ
+      this.title = [`上り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`, `下り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`];
+      this.flag = 0;
+    }
+    if (this.selectedItem == this.traffic[1]) {
+      this.title = this.train;
+    }
+    if (this.selectedItem == this.traffic[2]) {
+      this.title = this.bus;
+    }
+
+    for (let i = 0; i < this.train.length; i++) {
+      if (this.selectedItem == this.train[i]) {
+        //時刻表ファイルを読み込む
+        //テスト用のやつ
+        this.title = [`上り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`, `下り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`];
+        this.flag = 0;
+      }
+    }
+
+    for (let i = 0; i < this.bus.length; i++) {
+      if (this.selectedItem == this.bus[i]) {
+        //時刻表ファイルを読み込む
+        //テスト用のやつ
+        this.title = [`上り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`, `下り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`];
+        this.flag = 0;
+      }
+    }
+
+
+    this.items = [];
+    for (let i = 0; i < this.title.length; i++) {
+      this.items.push({
+        title: this.title[i],
+        note: ''
+      });
+    }
   }
 
-
   itemTapped(event, item, flag) {
-    this.navCtrl.push(TimeTablePage, {station:item});
+    if (flag) {
+      // That's right, we're pushing to ourselves!
+      this.navCtrl.push(Page4, { title: item });
+    }
   }
 }
