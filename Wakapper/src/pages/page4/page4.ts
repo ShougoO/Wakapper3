e23d80b3cb29f092470e6090c50f2820f83291ce
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-01-24 01:31:50 +0900

    [update]get data from json on page4
    gistに上げてるテスト用jsonファイルから取得するように変更
    その他使っていない変数の削除

--- Wakapper/src/pages/page4/page4.ts
+++ Wakapper/src/pages/page4/page4.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-import {TimeTablePage} from '../time-table/time-table'
+import {TimeTablePage} from '../time-table/time-table';
+import {JsonData} from '../../app/json-data';
 
 @Component({
   selector: 'page-page4',
@@ -15,26 +16,21 @@ export class Page4 {
   train: string[];
   bus: string[];
 
+  testJsonURL:string;
 
-  selectedItem: any;
-  data: Array<{ traffic: Number, place: Number }>;
+  constructor(public navCtrl: NavController, public navParams: NavParams,getJson:JsonData) {
+    this.testJsonURL = 'https://gist.githubusercontent.com/ueken0307/6d7205f2492a48cbc2c4bcdbeb2754b1/raw/d140717972a7efe8e0bbc04728e4356643ffae2e/station.json';
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.selectedItem = navParams.get('item');
-
-    //渡船場データ取得
-    this.boat = ['若松渡場', '戸畑渡場'];
-
-    //駅データ取得
-    this.train = ['若松駅', '奥洞海駅', '藤ノ木駅', '二島駅'];
-
-    //バス停データ取得
-    this.bus = ['バス停1', 'バス停2', 'バス停3', 'バス停4'];
-
+    getJson.getData(this.testJsonURL).subscribe(input=>{
+      //渡船場データ取得
+      this.boat = input.boat;
+      //駅データ取得
+      this.train = input.train;
+      //バス停データ取得
+      this.bus = input.bus;
+    });
 
   }
-
 
   itemTapped(event, item, flag) {
     this.navCtrl.push(TimeTablePage, {station:item});
