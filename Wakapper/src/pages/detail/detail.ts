15afd32e7b7789378d41958fdac185c3ef4fd1b0
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-03-07 10:21:17 +0900

    [update]update detail-page
    詳細ページの作成。
    ※要テスト

--- Wakapper/src/pages/detail/detail.ts
+++ Wakapper/src/pages/detail/detail.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import {JsonData} from '../../app/json-data';
+
 /**
  * Generated class for the DetailPage page.
  *
@@ -15,11 +17,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  traffic: string = "boat";
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DetailPage');
+  name:string;
+  address:string;
+  picURL:string;
+  open:string;
+  comment:Array<{title:string,comment:string}>;
+  commentSize:number;
+
+  //boat: string[];
+  //train: string[];
+  //bus: string[];
+
+  //testJsonURL:string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,getJson:JsonData) {
+    /*
+    this.testJsonURL = 'https://gist.githubusercontent.com/ueken0307/6d7205f2492a48cbc2c4bcdbeb2754b1/raw/d140717972a7efe8e0bbc04728e4356643ffae2e/station.json';
+
+    getJson.getData(this.testJsonURL).subscribe(input=>{
+      //渡船場データ取得
+      this.boat = input.boat;
+      //駅データ取得
+      this.train = input.train;
+      //バス停データ取得
+      this.bus = input.bus;
+    });
+    */
+
+    this.name = "マック 二島リバーウォーク店";
+    this.address = "福岡県北九州市若松区二島  １－４－７";
+    this.comment = [];
+
+    this.comment.push({
+      title: " かなしい",
+      comment: "へいてんした"
+    });
+
+    this.comment.push({
+      title: "たいとる",
+      comment: "おーぷんした"
+    });
+
+    this.open = `営業時間
+      平日:9:00~19:00
+      土日祝日:10:00~19:00    
+    `;
+
+    this.commentSize = this.comment.length;
   }
 
 }
